041f8b3057b4d345b7d4550672fd73820a9e9253
BrunaCelestin <brunaccelestino@gmail.com>
2023-09-16 13:37:00 -0300

    feat: initial project test

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import '../styles/index.css';
 import { ChakraProvider, Flex } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
-import React from 'react';
+
 import {theme} from "../styles/theme"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,10 +1,31 @@
 import { NextPage } from "next"
+import { Flex, Grid, GridItem, Image, Box } from '@chakra-ui/react';
 
 const Home: NextPage = () => {
-    return (
-        <>
-        hello</>
-    )
- }
+    const imageName = ["agua_copo", "banana", "banho", "bolinho", "bolofofo", "brinquedo", "danone_garrafa", "danoninho", "desenho", "livro", "lousa", "mundo_bita", "parquinho", "piscina", "suco"];
 
- export default Home;
+    const baseImageLocation = "/images/";
+    const baseAudioLocation = "/audios/";
+
+
+    const playAudio = (data: any) => {
+        const audio = new Audio(`${baseAudioLocation}${data}.m4a`)
+        audio.play()
+    }
+
+    return (
+        <Flex maxH="max-content" justifyContent="center"
+        w="100vw !important" margin="16px">
+            <Grid templateColumns='repeat(2, 1fr)' gap={4}>
+            {imageName.length !== 0 && imageName.map((data, i)=> (
+            <GridItem key={i} justifyContent="center" onClick={()=> playAudio(data)}>
+                <Flex cursor="pointer" borderRadius="16px" border="1px solid #E3E5E5"  direction="column" >
+                    <Image objectFit="fill" borderRadius="16px" maxH="500px"  src={`${baseImageLocation}${data}.jpg`} />
+                </Flex>
+                    </GridItem>))}
+            </Grid>
+          </Flex>
+    )
+}
+
+export default Home;
